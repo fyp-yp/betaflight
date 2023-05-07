b2a04e1d3e99fdffbb9e8b0fe18e3e36270d07cc
wxc <wxc@ulinor.com>
2023-05-07 22:13:10 +0800

    hide tabs

--- src/js/utils/updateTabList.js
+++ src/js/utils/updateTabList.js
@@ -11,15 +11,29 @@ export function updateTabList(features) {
         $('#tabs ul.mode-connected li.tab_servos').show();
         $('#tabs ul.mode-connected li.tab_sensors').show();
         $('#tabs ul.mode-connected li.tab_logging').show();
+        $('#tabs ul.mode-connected li.tab_pid_tuning').show();
+        $('#tabs ul.mode-connected li.tab_onboard_logging').show();
+        $('#tabs ul.mode-connected li.tab_auxiliary').show();
+        $('#tabs ul.mode-connected li.tab_ports').show();
+        $('#tabs ul.mode-connected li.tab_presets').show();
+        $('#tabs ul.mode-connected li.tab_configuration').show();
+        $('#tabs ul.mode-connected li.tab_cli').show();
     } else {
         $('#tabs ul.mode-connected li.tab_failsafe').hide();
         $('#tabs ul.mode-connected li.tab_adjustments').hide();
         $('#tabs ul.mode-connected li.tab_servos').hide();
         $('#tabs ul.mode-connected li.tab_sensors').hide();
         $('#tabs ul.mode-connected li.tab_logging').hide();
+        $('#tabs ul.mode-connected li.tab_pid_tuning').hide();
+        $('#tabs ul.mode-connected li.tab_onboard_logging').hide();
+        $('#tabs ul.mode-connected li.tab_auxiliary').hide();
+        $('#tabs ul.mode-connected li.tab_ports').hide();
+        $('#tabs ul.mode-connected li.tab_presets').hide();
+        $('#tabs ul.mode-connected li.tab_configuration').hide();
+        $('#tabs ul.mode-connected li.tab_cli').hide();
     }
 
-    if (features.isEnabled('GPS') && isExpertModeEnabled()) {
+    if (features.isEnabled('GPS')) {
         $('#tabs ul.mode-connected li.tab_gps').show();
     } else {
         $('#tabs ul.mode-connected li.tab_gps').hide();
@@ -31,7 +45,7 @@ export function updateTabList(features) {
         $('#tabs ul.mode-connected li.tab_led_strip').hide();
     }
 
-    if (features.isEnabled('TRANSPONDER')) {
+    if (features.isEnabled('TRANSPONDER') && isExpertModeEnabled()) {
         $('#tabs ul.mode-connected li.tab_transponder').show();
     } else {
         $('#tabs ul.mode-connected li.tab_transponder').hide();
@@ -43,9 +57,13 @@ export function updateTabList(features) {
         $('#tabs ul.mode-connected li.tab_osd').hide();
     }
 
-    $('#tabs ul.mode-connected li.tab_power').show();
+    if (isExpertModeEnabled()) {
+        $('#tabs ul.mode-connected li.tab_power').show();
+    } else {
+        $('#tabs ul.mode-connected li.tab_power').hide();
+    }
 
-    if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_42)) {
+    if (semver.gte(FC.CONFIG.apiVersion, API_VERSION_1_42) && isExpertModeEnabled()) {
         $('#tabs ul.mode-connected li.tab_vtx').show();
     } else {
         $('#tabs ul.mode-connected li.tab_vtx').hide();
